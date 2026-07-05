d8a561e340014ee0e72371a73a1df99f8e0ffb29
Francis Jandrei Muñoz <francisjandrei31@gmail.com>
2026-07-06 02:55:59 +0800

    Update bern_BMI-Calculator.py

--- basic-python-builds/bern_BMI-Calculator.py
+++ basic-python-builds/bern_BMI-Calculator.py
@@ -5,21 +5,18 @@
 # Cast the inputs to floats, calculate the BMI, and print the result.
 # Formula: BMI = weight in kg / (height in meters * height in meters)
 
-weight = float(input("Enter your Weight (in Kg): "))
-height = float(input("Enter your Height (in Meters): "))
+weight = float(input("Enter your Weight (in Kg): ")) #asks for weight input in float value (decimals included)
+height = float(input("Enter your Height (in Meters): ")) #asks for height input in float value (decimals included)
 
-total_bmi = weight / (height * height)
+total_bmi = weight / (height * height) #calculates BMI with given formula (mathematically, weight divided by height^2)
 
-## will use better function to limit inputs to only Kg and m soon
-if weight >= 635 or height >= 3: 
+if (weight >= 635 or weight < 0) or (height >= 3 or height < 0): #will use better function to limit inputs to only Kg and m soon
     print("Invalid Value")
-elif weight <= 635 or height <= 3:
-    print("Your BMI is: ", total_bmi)
-    if total_bmi < 18.5:
-        print("Underweight")
-    elif 18.5 <= total_bmi <= 24.9:
-        print("Normal")
-    elif 25.0 <= total_bmi <= 29.9:
-        print("Overweight")
-    else:
-        print("Obese")
+elif total_bmi < 18.5: #value from total_value variable compared to set threshold
+    print("Your BMI is: ",total_bmi, "Underweight") #print combination of BMI result, total_bmi variable stored value, string value for set threshold
+elif 18.5 <= total_bmi <= 24.9: #value from total_value variable compared to set threshold
+    print("Your BMI is: ",total_bmi, "Normal") #print combination of BMI result, total_bmi variable stored value, string value for set threshold
+elif 25.0 <= total_bmi <= 29.9: #value from total_value variable compared to set threshold
+    print("Your BMI is: ",total_bmi, "Overweight") #print combination of BMI result, total_bmi variable stored value, string value for set threshold
+else: #value from what's left, >30 compared to set threshold
+    print("Your BMI is: ",total_bmi, "Obese") #print combination of BMI result, total_bmi variable stored value, string value for set threshold
